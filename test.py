bbc504378e99c6e1943016eccb8940abae28131f
Heather McGrath <heathermcgrath@gmail.com>
2021-07-18 23:14:55 +0100

    create dataframes, print info

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 
 import pandas as pd
 import numpy as np
-df=pd.read_csv("Residential Property Price Index.csv")
-print(df)
+RPPI=pd.read_csv("Residential Property Price Index.csv")
+
+UNEMP=pd.read_csv("Seasonally Adjusted Monthly Unemployment.csv")
+
+UNEMPC19=pd.read_csv("Covid-19 Adjusted Monthly Unemployment Estimates.csv")
+
+print(RPPI.info())
+print(UNEMP.info())
+print(UNEMPC19.info())
+
+
+
 
 
 
