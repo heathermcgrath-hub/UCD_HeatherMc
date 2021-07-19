8dfc559f4cb3cc254c273df3213592dc3a522824
Heather McGrath <heathermcgrath@gmail.com>
2021-07-19 22:49:32 +0100

    Subsetting, sorting and indexing data

--- test.py
+++ test.py
@@ -36,7 +36,29 @@ print(RPPI_DROPDUPES.info())
 print(UNEMP_DROPDUPES.info())
 print(UNEMPC19_DROPDUPES.info())
 
+#subsetting sorting and indexing RPPI data
+print(RPPI_DROPDUPES.head())
+print(RPPI_DROPDUPES.columns)
+RPPI_DROPDUPES_FILTERED= RPPI_DROPDUPES[ (RPPI_DROPDUPES["Statistic"] == "Percentage Change over 1 month for Residential Property Price Index") & (RPPI_DROPDUPES["Type of Residential Property"] == "National - all residential properties")]
+print(RPPI_DROPDUPES_FILTERED.info())
+RPPI_SORTED=RPPI_DROPDUPES_FILTERED.sort_values("Month",ascending=True)
+RPPI_SORTED_IND=RPPI_SORTED.set_index("Month")
 
+#subsetting sorting and indexing UNEMP data
+print(UNEMP_DROPDUPES.head())
+print(UNEMP_DROPDUPES.columns)
+UNEMP_DROPDUPES_FILTERED = UNEMP_DROPDUPES[(UNEMP_DROPDUPES["Statistic"] == "Seasonally Adjusted Monthly Unemployment Rate") & (UNEMP_DROPDUPES["Age Group"] == "25 - 74 years") & (UNEMP_DROPDUPES["Sex"] == "Both sexes")]
+print(UNEMP_DROPDUPES_FILTERED.info())
+UNEMP_SORTED = UNEMP_DROPDUPES_FILTERED.sort_values("Month", ascending=True)
+UNEMP_SORTED_IND = UNEMP_SORTED.set_index("Month")
+
+#subsetting sorting and indexing UNEMPC19 data
+print(UNEMPC19_DROPDUPES.head())
+print(UNEMPC19_DROPDUPES.columns)
+UNEMPC19_DROPDUPES_FILTERED = UNEMPC19_DROPDUPES[(UNEMPC19_DROPDUPES["Statistic"] == "Monthly Unemployment Rate") & (UNEMPC19_DROPDUPES["Age Group"] == "25 - 74 years") & (UNEMPC19_DROPDUPES["Sex"] == "Both sexes") & (UNEMPC19_DROPDUPES["Lower and Upper Bound"] == "Upper Bound (COVID-19 Adjusted MUR)")]
+print(UNEMPC19_DROPDUPES_FILTERED.info())
+UNEMPC19_SORTED = UNEMPC19_DROPDUPES_FILTERED.sort_values("Month", ascending=True)
+UNEMPC19_SORTED_IND = UNEMPC19_SORTED.set_index("Month")
 
 
 
